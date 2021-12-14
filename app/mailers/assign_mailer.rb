1a0d262741e0ffaf38a6a348879dbc1149d2ce87
ootsukasyouya <rikkuhose@gmail.com>
2021-12-14 15:40:25 +0000

    トイ中

--- app/mailers/assign_mailer.rb
+++ app/mailers/assign_mailer.rb
@@ -1,10 +1,17 @@
 class AssignMailer < ApplicationMailer
   default from: 'from@example.com'
+  default from: 'rikkuhose@gmail.com'
 
   def assign_mail(email, password)
     @email = email
     @password = password
     mail to: @email, subject: I18n.t('views.messages.complete_registration')
+  end
+  
+  def appoint_mail(assign)
+    @assign = assign
+    @team_info = Team.where(owner_id:@assign.id)
+    mail to: @assign.email, subject: "あなたはチームのリーダーになりました"
   end
   
   def del_agenda_mail(user)
